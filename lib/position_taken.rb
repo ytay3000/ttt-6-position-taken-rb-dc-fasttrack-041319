1e24332e63e22e30360ddebccd9ed82cb6167bc0
Yair Taylor <yairtaylor@gmail.com>
2019-04-14 02:17:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if (board[index] == " " || board[index] == "" || index.nil?)
+  if index.nil?
+    return FALSE
+  elsifif (board[index] == " " || board[index] == "")
     return FALSE
   else
     return TRUE
